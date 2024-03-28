0ff131cd87f1ed783a256207b5fb5af42b0a3718
woshiren <yejiacong@outlook.com>
2024-03-28 20:39:12 +0800

    update Guess_number

--- basic/src/shape_no_virtual_main.cpp
+++ basic/src/shape_no_virtual_main.cpp
@@ -5,14 +5,25 @@
 #include <iostream>
 
 int main()
-{
-    Circle circle(1.0);
-    Rectangle rectangle(1.0, 2.0);
-    Square square(1.0);
+{\
+    double radius;
+    std::cout<<"Please input the radius of the circle: \n";
+    std::cin>>radius;
+    Circle circle(radius);
+
+    double width, height;
+    std::cout<<"Please input the width and the height of the rectangle: [w h]\n";
+    std::cin>>width>>height;
+    Rectangle rectangle(width, height);
+
+    double side;
+    std::cout<<"Please input the side of the square: \n";
+    std::cin>>side;
+    Square square(side);
 
     std::cout<< "Area of circle: " << circle.area() << std::endl;
     std::cout<< "Area of rectangle: " << rectangle.area() << std::endl;
     std::cout<< "Area of square: " << square.area() << std::endl;
-    
+
     return 0;
 }
