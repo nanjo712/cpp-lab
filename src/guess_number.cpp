b46e9a10476e0a078a91cbecb100f0732005c8c1
woshiren <yejiacong@outlook.com>
2024-04-11 14:55:24 +0800

    去掉乱码

--- src/guess_number.cpp
+++ src/guess_number.cpp
@@ -7,22 +7,22 @@ int main()
     std::default_random_engine engine(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
     std::uniform_int_distribution<int> distribution(1, 1000);
     int random_number = distribution(engine);
-    std::cout << "��Ʒ�۸������ɣ���ΧΪ[1,1000), ��²�۸�";
+    std::cout << "商品价格已生成，范围为[1,1000), 请猜测价格：";
     int guess_number = -1;
     while (guess_number != random_number)
     {
         std::cin >> guess_number;
         if (guess_number > random_number)
         {
-            std::cout << "�´��ˣ����������룺";
+            std::cout << "猜大了，请重新输入：";
         }
         else if (guess_number < random_number)
         {
-            std::cout << "��С�ˣ����������룺";
+            std::cout << "猜小了，请重新输入：";
         }
         else
         {
-            std::cout << "��ϲ�㣬�¶��ˣ�" << std::endl;
+            std::cout << "恭喜你，猜对了！" << std::endl;
         }
     }
     return 0;
