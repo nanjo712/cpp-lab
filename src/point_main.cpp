f2630af7a019e9c24b630c3c2e259c77d0f10cbc
woshiren <yejiacong@outlook.com>
2024-04-11 21:03:53 +0800

    修改了可选输入

--- src/point_main.cpp
+++ src/point_main.cpp
@@ -5,12 +5,21 @@ int main()
 {
     std::cout << "Input the coordinates of the point" << std::endl;
     double x, y;
-    std::cin >> x >> y;
-    Point p(x, y);
+    Point *p;
+    if (std::cin >> x >> y)
+    {
+        p = new Point(x, y);
+    }
+    else
+    {
+        p = new Point();
+    }
 
-    std::cout << "p++: " << p++ << std::endl;
-    std::cout << "++p: " << ++p << std::endl;
-    std::cout << "p--: " << p-- << std::endl;
-    std::cout << "--p: " << --p << std::endl;
+    std::cout << "p++: " << (*p)++ << std::endl;
+    std::cout << "++p: " << ++(*p) << std::endl;
+    std::cout << "p--: " << (*p)-- << std::endl;
+    std::cout << "--p: " << --(*p) << std::endl;
+
+    delete p;
     return 0;
 }
